26855d5243eb1163ebd38343c4faa7b49193ebf1
elisam98 <lineage@safetelecom.net>
2018-11-22 22:01:37 -0500

    Integrate JasonM's perry mods

--- lineage.mk
+++ lineage.mk
@@ -25,9 +25,7 @@ PRODUCT_NAME := lineage_perry
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=motorola/perry/perry:7.1.1/NPNS26.118-22-1/1:user/release-keys \
+    BUILD_FINGERPRINT="motorola/perry/perry:7.1.1/NPNS26.118-22-1/1:user/release-keys" \
     PRIVATE_BUILD_DESC="perry-user 7.1.1 NPNS26.118-22-1 1 release-keys" \
     PRODUCT_NAME="Moto E4"
