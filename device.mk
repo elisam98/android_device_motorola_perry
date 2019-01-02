9e9ffb0cc03ba1ed23b918651c7ffdde97e4f3b4
elisam98 <cornbeefonrye@hotmail.com>
2019-01-01 21:38:21 -0500

    Revert "trying video playback fixes"
    This reverts commit dfbe6d62f3b965640684a03543f58ec89c8bd2f6.

--- device.mk
+++ device.mk
@@ -42,13 +42,6 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
-# Media
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.target.rc
